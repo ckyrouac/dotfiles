a28de63780f5726b934510bb1a39cfd14f3abc4e
Chris Kyrouac <ckyrouac@redhat.com>
2015-09-21 12:07:03 -0400

    initial setup.sh script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#symlinks
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/bashrc ~/.bashrc
+
+#vim submodules
+git submodule init
+git submodule update
+cd bundle/YouCompleteMe
+sudo yum install python-devel cmake
+git submodule update --init --recursive
+cd ~/
+mkdir ycm_build
+cd ycm_build
+cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+make ycm_support_libs
