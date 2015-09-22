ce5d924b7ebfac3b5e595046f04d890f877d473b
ckyrouac <ckyrouac@redhat.com>
2015-09-22 14:29:42 -0400

    rvm bug fix in setup.sh

--- setup.sh
+++ setup.sh
@@ -85,7 +85,7 @@ function setup-git () {
 function setup-ruby () {
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   curl -sSL https://get.rvm.io | bash -s stable
-  source ~/.rvm/rvm.sh
+  source /etc/profile.d/rvm.sh
   rvm install 2.1.1
   rvm use 2.1.1 --default
 }
