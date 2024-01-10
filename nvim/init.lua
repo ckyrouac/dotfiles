b31da1c161af05824c2fbd0da3cb0f42cf0307b4
ckyrouac <ckyrouac@redhat.com>
2024-01-10 09:10:40 -0500

    Highlight vim search, icons enabled

--- nvim/init.lua
+++ nvim/init.lua
@@ -197,7 +197,7 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'onedark',
         component_separators = '|',
         section_separators = '',
@@ -267,7 +267,7 @@ require('lazy').setup({
 -- NOTE: You can change these options as you wish!
 
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.o.hlsearch = true
 
 -- Make line numbers default
 vim.wo.number = true
@@ -463,6 +463,8 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+
+vim.keymap.set('n', '', ':noh<CR>', { desc = 'Clear search highlight' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
