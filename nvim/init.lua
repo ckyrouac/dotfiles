b6adb5ee89b84ebf7ec9a8e274e907ef82ff34ce
ckyrouac <chriskyrouac@gmail.com>
2024-01-14 11:20:20 -0500

    Vim color tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,7 +39,11 @@ require('lazy').setup({
   {'nvim-tree/nvim-tree.lua', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
 
   -- Buffer tabs
-  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons'
+  },
   'Asheq/close-buffers.vim',
 
   -- Session management
@@ -267,6 +271,8 @@ require('lazy').setup({
 
 -- Set highlight on search
 vim.o.hlsearch = true
+vim.cmd('hi CurSearch guifg=#262627 guibg=#ffc66b')
+vim.cmd('hi Search guifg=#a0a0a0 guibg=#505052')
 
 -- Make line numbers default
 vim.wo.number = true
@@ -732,8 +738,200 @@ require("nvim-tree").setup({
   },
 })
 
+local bufferline_tab_bg_color='#212121'
+
 -- Buffer tabs
-require("bufferline").setup{}
+require("bufferline").setup({
+  options = {
+    offsets = {
+        {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+            separator = true
+        }
+    },
+  },
+  highlights = {
+    tab = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- tab_selected = {
+    --     fg = '<colour-value-here>',
+    --     bg = '<colour-value-here>',
+    -- },
+    close_button_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- close_button_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    buffer_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- buffer_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    numbers = {
+        bg = bufferline_tab_bg_color,
+    },
+    numbers_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- numbers_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    diagnostic = {
+        bg = bufferline_tab_bg_color,
+    },
+    diagnostic_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- diagnostic_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    hint = {
+        bg = bufferline_tab_bg_color,
+    },
+    hint_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- hint_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    hint_diagnostic = {
+        bg = bufferline_tab_bg_color,
+    },
+    hint_diagnostic_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- hint_diagnostic_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    info = {
+        bg = bufferline_tab_bg_color,
+    },
+    info_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- info_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    info_diagnostic = {
+        bg = bufferline_tab_bg_color,
+    },
+    info_diagnostic_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- info_diagnostic_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    warning = {
+        bg = bufferline_tab_bg_color,
+    },
+    warning_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- warning_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    warning_diagnostic = {
+        bg = bufferline_tab_bg_color,
+    },
+    warning_diagnostic_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- warning_diagnostic_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    error = {
+        bg = bufferline_tab_bg_color,
+    },
+    error_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- error_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    error_diagnostic = {
+        bg = bufferline_tab_bg_color,
+    },
+    error_diagnostic_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- error_diagnostic_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    modified_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- modified_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    -- duplicate_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    duplicate_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    duplicate = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- separator_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    separator_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    separator = {
+        bg = bufferline_tab_bg_color,
+    },
+    indicator_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    -- indicator_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    -- pick_selected = {
+    --     bg = bufferline_tab_bg_color,
+    -- },
+    pick_visible = {
+        bg = bufferline_tab_bg_color,
+    },
+    pick = {
+        bg = bufferline_tab_bg_color,
+    },
+    offset_separator = {
+        bg = bufferline_tab_bg_color,
+    },
+    trunc_marker = {
+        bg = bufferline_tab_bg_color,
+    },
+
+    -- done
+    fill = {
+      bg = '#292929',
+    },
+    background = {
+      bg = bufferline_tab_bg_color,
+    },
+    close_button = {
+      bg = bufferline_tab_bg_color,
+    },
+    tab_close = {
+      bg = bufferline_tab_bg_color,
+    },
+    tab_separator = {
+      bg = bufferline_tab_bg_color,
+    },
+    -- tab_separator_selected = {
+    --   bg = bufferline_tab_bg_color,
+    -- },
+    modified = {
+      bg = bufferline_tab_bg_color,
+    }
+  }
+})
 
 -- Session management
 require("auto-session").setup{}
@@ -793,6 +991,17 @@ require('lspconfig').lua_ls.setup {
       },
     },
   },
+}
+
+-- lualine theme
+local lualine_theme = require'lualine.themes.auto'
+lualine_theme.normal.c.bg = '#292929'
+lualine_theme.insert.c.bg = '#292929'
+lualine_theme.visual.c.bg = '#292929'
+lualine_theme.command.c.bg = '#292929'
+
+require('lualine').setup {
+  options = { theme  = lualine_theme },
 }
 
 -- vim.lsp.set_log_level("debug")
