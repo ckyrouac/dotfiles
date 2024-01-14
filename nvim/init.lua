b763fb7701a1b93cc65c906d827f3110ad042755
ckyrouac <chriskyrouac@gmail.com>
2024-01-14 11:23:49 -0500

    Tweak search highlight in vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -271,7 +271,7 @@ require('lazy').setup({
 
 -- Set highlight on search
 vim.o.hlsearch = true
-vim.cmd('hi CurSearch guifg=#262627 guibg=#ffc66b')
+vim.cmd('hi CurSearch guifg=#505052 guibg=#a0a0a0')
 vim.cmd('hi Search guifg=#a0a0a0 guibg=#505052')
 
 -- Make line numbers default
