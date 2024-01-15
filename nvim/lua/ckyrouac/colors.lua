685202cf86b857aebc216f558c0f0a3f0af852c7
ckyrouac <chriskyrouac@gmail.com>
2024-01-15 09:44:18 -0500

    Nvim-tree cleanup

--- nvim/lua/ckyrouac/colors.lua
+++ nvim/lua/ckyrouac/colors.lua
@@ -1,3 +1,18 @@
 -- Override plugin colors here. This is loaded last.
 vim.cmd [[ hi! Search guifg=#a0a0a0 guibg=#505052 ]]
 vim.cmd [[ hi! CurSearch guifg=#505052 guibg=#a0a0a0 ]]
+
+
+vim.api.nvim_create_autocmd({"BufEnter","FocusGained","WinEnter"}, {
+  pattern = {"*NvimTree*"},
+  callback = function()
+    vim.cmd [[ hi! NvimTreeNormal guibg=#262627 ]]
+  end
+})
+
+vim.api.nvim_create_autocmd({"BufLeave","FocusLost","WinLeave"}, {
+  pattern = {"*NvimTree*"},
+  callback = function()
+    vim.cmd [[ hi! NvimTreeNormal guibg=#232324 ]]
+  end
+})
--- nvim/lua/ckyrouac/plugins/nvim-tree.lua
+++ nvim/lua/ckyrouac/plugins/nvim-tree.lua
@@ -9,13 +9,13 @@ return {
           sorter = "case_sensitive",
         },
         view = {
-          width = 30,
+          width = 50,
         },
         renderer = {
           group_empty = true,
         },
         filters = {
-          dotfiles = true,
+          dotfiles = false,
         },
       })
 
