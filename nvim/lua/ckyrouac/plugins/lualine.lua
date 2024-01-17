4481d0e4c016f21575a2f35ea89432ffef738853
ckyrouac <ckyrouac@redhat.com>
2024-01-16 21:07:54 -0500

    Update lualine colors

--- nvim/lua/ckyrouac/plugins/lualine.lua
+++ nvim/lua/ckyrouac/plugins/lualine.lua
@@ -14,10 +14,10 @@ return {
     config = function ()
       -- lualine theme
       local lualine_theme = require'lualine.themes.auto'
-      lualine_theme.normal.c.bg = '#292929'
+      lualine_theme.normal.c.bg = '#2b2d30'
       lualine_theme.insert.c.bg = '#292929'
-      lualine_theme.visual.c.bg = '#292929'
-      lualine_theme.command.c.bg = '#292929'
+      lualine_theme.visual.c.bg = '#2b2d30'
+      lualine_theme.command.c.bg = '#2b2d30'
 
       require('lualine').setup {
         options = { theme  = lualine_theme },
