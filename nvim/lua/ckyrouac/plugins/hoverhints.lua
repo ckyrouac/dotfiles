60cd47d3e2198f087264648bb3956eca40668e0c
ckyrouac <ckyrouac@redhat.com>
2024-01-17 12:29:19 -0500

    Disable hoverhints

--- nvim/lua/ckyrouac/plugins/hoverhints.lua
+++ nvim/lua/ckyrouac/plugins/hoverhints.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    'ckyrouac/hoverhints.nvim',
-    config = function ()
-      require("hoverhints").setup({
-        scrollbar_offset = 0
-      })
-    end
-  }
+  -- {
+  --   'ckyrouac/hoverhints.nvim',
+  --   config = function ()
+  --     require("hoverhints").setup({
+  --       scrollbar_offset = 0
+  --     })
+  --   end
+  -- }
 }
