80a627cdfc73cde65dff65ac00d016c9e8a6df21
ckyrouac <ckyrouac@redhat.com>
2024-01-17 05:57:38 -0500

    Find in home dir shortcut

--- nvim/lua/ckyrouac/plugins/telescope.lua
+++ nvim/lua/ckyrouac/plugins/telescope.lua
@@ -88,10 +88,15 @@ return {
         }
       end
 
+      local function find_in_home_dir()
+        require('telescope.builtin').find_files({ cwd = '~/', hidden = true })
+      end
+
       -- Telescope search keybinds
       vim.keymap.set('n', '<leader>t', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<A-t>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<C-f>', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<M-C-F>', find_in_home_dir, { desc = 'Search entire home directory' })
 
       vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
       vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
