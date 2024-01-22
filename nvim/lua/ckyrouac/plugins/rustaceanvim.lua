7c851bbac6ad04a3c0d80a54009f8a0373be4d7a
ckyrouac <chriskyrouac@gmail.com>
2024-01-22 12:12:41 -0500

    stash

--- nvim/lua/ckyrouac/plugins/rustaceanvim.lua
+++ nvim/lua/ckyrouac/plugins/rustaceanvim.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^3", -- Recommended
-    ft = { "rust" },
-    config = function()
-      vim.g.rustaceanvim = {
-        -- Plugin configuration
-        tools = {
-          hover_actions = {
-            auto_focus = true,
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   "mrcjkb/rustaceanvim",
+  --   version = "^3", -- Recommended
+  --   ft = { "rust" },
+  --   config = function()
+  --     vim.g.rustaceanvim = {
+  --       -- Plugin configuration
+  --       tools = {
+  --         hover_actions = {
+  --           auto_focus = true,
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
 }
