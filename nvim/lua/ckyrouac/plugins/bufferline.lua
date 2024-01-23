c4ec7e69e82600672dc47f5cb4970ab196db175b
ckyrouac <chriskyrouac@gmail.com>
2024-01-23 09:04:36 -0500

    Diagnostics in bufferline

--- nvim/lua/ckyrouac/plugins/bufferline.lua
+++ nvim/lua/ckyrouac/plugins/bufferline.lua
@@ -4,8 +4,24 @@ return {
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
+      local colors = require('darcula-solid.colors')
       require("bufferline").setup({
         options = {
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(_, level)
+            local signs = require('darcula-solid.diagnostics').Signs
+
+            if level:match("error") then
+              return signs.Error
+            elseif level:match("warn") then
+              return signs.Warn
+            elseif level:match("hint") then
+              return signs.Hint
+            elseif level:match("info") then
+              return signs.Info
+            end
+
+          end,
           close_command = "SmartQ %d",
           offsets = {
             {
@@ -28,8 +44,32 @@ return {
         },
         highlights = {
           background = {
-            bg = "#1e1f22",
+            bg = colors.bg,
           },
+          error = {
+            fg = colors.comment,
+            bg = colors.bg,
+          },
+          error_visible = {
+            fg = colors.comment,
+            bg = colors.bg,
+          },
+          error_selected = {
+            fg = colors.fg,
+            bg = colors.bg,
+          },
+          -- warning = {
+          --   fg = colors.fg,
+          --   bg = colors.bg,
+          -- },
+          warning_visible = {
+            fg = colors.comment,
+            bg = colors.bg,
+          },
+          warning_selected = {
+            fg = colors.fg,
+            bg = colors.bg,
+          }
         },
       })
     end,
