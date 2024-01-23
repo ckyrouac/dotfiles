64259bf77b724257f8b2da5c305b17c622f96911
ckyrouac <ckyrouac@redhat.com>
2024-01-23 12:07:55 -0500

    Speed up neovide cursor animation

--- nvim/lua/ckyrouac/neovide.lua
+++ nvim/lua/ckyrouac/neovide.lua
@@ -17,8 +17,8 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_scroll_animation_far_lines = 0
 
-  vim.g.neovide_cursor_animation_length = 0.05
-  vim.g.neovide_cursor_trail_size = 0.4
+  vim.g.neovide_cursor_animation_length = 0.04
+  vim.g.neovide_cursor_trail_size = 0.3
   vim.g.neovide_cursor_animate_in_insert_mode = false
   vim.g.neovide_cursor_animate_command_line = true
   vim.g.neovide_cursor_vfx_mode = false
