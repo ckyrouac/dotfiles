3eb94c6a3971ad299873fab4a838fa1f2fb7cb1e
ckyrouac <chriskyrouac@gmail.com>
2024-01-24 07:44:12 -0500

    Noice cleanup

--- nvim/lua/ckyrouac/plugins/noice.lua
+++ nvim/lua/ckyrouac/plugins/noice.lua
@@ -94,7 +94,12 @@ return {
             },
           },
           messages = {
-            enabled = true,
+            enabled = true, -- enables the Noice messages UI
+            view = "mini", -- default view for messages
+            view_error = "notify", -- view for errors
+            view_warn = "notify", -- view for warnings
+            view_history = "messages", -- view for :messages
+            view_search = "virtualtext",
           },
           lsp = {
             hover = {
