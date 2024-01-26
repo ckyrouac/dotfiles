d0ba36dba6b044ce023cc790c6ac4ba41823a445
ckyrouac <chriskyrouac@gmail.com>
2024-01-26 08:06:45 -0500

    Fix DAP project config loading

--- nvim/lua/ckyrouac/plugins/dap/nvim-dap-projects.lua
+++ nvim/lua/ckyrouac/plugins/dap/nvim-dap-projects.lua
@@ -2,7 +2,6 @@ return {
   {
     "ldelossa/nvim-dap-projects",
     config = function()
-      require("nvim-dap-projects").search_project_config()
     end,
   },
 }
--- nvim/lua/ckyrouac/plugins/neovim-project.lua
+++ nvim/lua/ckyrouac/plugins/neovim-project.lua
@@ -11,6 +11,16 @@ return {
     init = function()
       -- enable saving the state of plugins in the session
       vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+      local augroup = vim.api.nvim_create_augroup("user_cmds", { clear = true })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = { "SessionLoadPost" },
+        group = augroup,
+        desc = "Update git env for dotfiles after loading session",
+        callback = function()
+          require("nvim-dap-projects").search_project_config()
+        end,
+      })
     end,
     dependencies = {
       { "nvim-lua/plenary.nvim" },
