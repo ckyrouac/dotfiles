06e11a38799941e0891534fa1bd1bbb7cecefb89
ckyrouac <chriskyrouac@gmail.com>
2024-01-27 15:34:58 -0500

    Cleanup overseer unused var

--- nvim/lua/ckyrouac/plugins/overseer.lua
+++ nvim/lua/ckyrouac/plugins/overseer.lua
@@ -3,8 +3,7 @@ return {
     "stevearc/overseer.nvim",
     lazy = false,
     config = function()
-      local overseer = require("overseer").setup()
-
+      require("overseer").setup()
       vim.keymap.set("n", "<leader>ot", "<cmd>OverseerToggle<cr>", { silent = true, desc = "Overseer Tasks" })
     end,
   },
