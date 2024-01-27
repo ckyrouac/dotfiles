0c9911f2fe1876a6090386d8bfc96c07b4580ca1
ckyrouac <chriskyrouac@gmail.com>
2024-01-27 15:33:45 -0500

    Cleanup dap/overseer config

--- nvim/lua/ckyrouac/plugins/dap/nvim-dap.lua
+++ nvim/lua/ckyrouac/plugins/dap/nvim-dap.lua
@@ -1,9 +1,5 @@
 return {
   {
     "mfussenegger/nvim-dap",
-    config = function ()
-      require("dap.ext.vscode").load_launchjs()
-      require("overseer").patch_dap(true)
-    end
   },
 }
--- nvim/lua/ckyrouac/plugins/overseer.lua
+++ nvim/lua/ckyrouac/plugins/overseer.lua
@@ -3,11 +3,7 @@ return {
     "stevearc/overseer.nvim",
     lazy = false,
     config = function()
-      require("overseer").setup({
-      local overseer = require("overseer")
-      overseer.setup({
-        dap = false, --patch dap in dap setup
-      })
+      local overseer = require("overseer").setup()
 
       vim.keymap.set("n", "<leader>ot", "<cmd>OverseerToggle<cr>", { silent = true, desc = "Overseer Tasks" })
     end,
