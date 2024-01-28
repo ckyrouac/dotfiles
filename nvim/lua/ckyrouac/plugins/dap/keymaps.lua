c09c0fa8cf27cf1fe49501583f59464597cbf9c4
ckyrouac <chriskyrouac@gmail.com>
2024-01-28 08:41:26 -0500

    Add dap select config keymap

--- nvim/lua/ckyrouac/plugins/dap/keymaps.lua
+++ nvim/lua/ckyrouac/plugins/dap/keymaps.lua
@@ -1,3 +1,36 @@
+local function flatten_dap_configurations()
+  local configurations = require('dap').configurations
+  local flat_configs = {}
+
+  for type, configs in pairs(configurations) do
+    for _, conf in ipairs(configs) do
+      conf.file_type = type
+      table.insert(flat_configs, conf)
+    end
+  end
+
+  return flat_configs
+end
+
+local function select_config_and_run(opts)
+  local configurations = flatten_dap_configurations()
+  opts = opts or {}
+  require('dap.ui').pick_one(
+    configurations,
+    "Configuration: ",
+    function(i) return i.file_type .. ": " .. i.name end,
+    function(configuration)
+      if configuration then
+        require('dap').run(configuration, opts)
+      else
+        vim.notify('No configuration selected', vim.log.levels.INFO)
+      end
+    end
+  )
+end
+vim.keymap.set("n", "<F10>", select_config_and_run, { silent = true, desc = 'DAP Select Config' })
+
+
 -- function keys
 vim.keymap.set("n", "<F4>", ":lua require('dapui').toggle()<CR>", { silent = true, desc = 'Toggle DAP UI' })
 vim.keymap.set("n", "<S-F4>", ":lua require('dapui').open({reset=true})<CR>", { silent = true, desc = 'Reset DAP UI' })
