084ee0ad7034be8ffff712ae13d117e4f29862af
ckyrouac <ckyrouac@redhat.com>
2024-02-02 13:52:58 -0500

    Add nvim-dap-projects refresh keybind

--- nvim/lua/ckyrouac/plugins/dap/nvim-dap-projects.lua
+++ nvim/lua/ckyrouac/plugins/dap/nvim-dap-projects.lua
@@ -2,7 +2,10 @@ return {
   {
     "ldelossa/nvim-dap-projects",
     cond = true,
+    event = "VeryLazy",
     config = function()
+      require('nvim-dap-projects').search_project_config()
+      vim.keymap.set("n", "<leader>dp", require('nvim-dap-projects').search_project_config, { silent = true, desc = "Reload project config" })
     end,
   },
 }
