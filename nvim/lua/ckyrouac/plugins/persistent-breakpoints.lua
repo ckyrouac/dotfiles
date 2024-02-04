6cabc9336990791268619551c657eecb326d9ac3
ckyrouac <chriskyrouac@gmail.com>
2024-02-04 08:20:05 -0500

    Use my fork of persistent-breakpoints

--- nvim/lua/ckyrouac/plugins/persistent-breakpoints.lua
+++ nvim/lua/ckyrouac/plugins/persistent-breakpoints.lua
@@ -1,7 +1,8 @@
 return {
   {
-    "Weissle/persistent-breakpoints.nvim",
+    "ckyrouac/persistent-breakpoints.nvim",
     cond = true,
+    lazy = false,
     config = function()
       require("persistent-breakpoints").setup({
         load_breakpoints_event = { "BufReadPost" },
