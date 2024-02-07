54a4667c902a3ecdb600a0a2ed9bcaa8e6f58f58
ckyrouac <ckyrouac@redhat.com>
2024-02-07 15:59:08 -0500

    Highlight references under cursor

--- nvim/lua/ckyrouac/plugins/nvim-lspconfig.lua
+++ nvim/lua/ckyrouac/plugins/nvim-lspconfig.lua
@@ -162,6 +162,27 @@ return {
         --   }
         -- }
       })
+
+      vim.api.nvim_create_autocmd({ "CursorHold" }, {
+        pattern = { "*" },
+        callback = function()
+          vim.lsp.buf.document_highlight()
+        end
+      })
+
+      vim.api.nvim_create_autocmd({ "CursorHoldI" }, {
+        pattern = { "*" },
+        callback = function()
+          vim.lsp.buf.document_highlight()
+        end
+      })
+
+      vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+        pattern = { "*" },
+        callback = function()
+          vim.lsp.buf.clear_references()
+        end
+      })
     end,
   },
 }
