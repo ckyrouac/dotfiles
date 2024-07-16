3badfeb6d4e2bf3d86292875414ac2fa48c3fa47
Chris Kyrouac <ckyrouac@redhat.com>
2024-07-16 13:39:55 -0400

    Add nvim-lint plugin

---
+++ nvim/lua/ckyrouac/plugins/nvim-lint.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    cond = true,
+    dependencies = {},
+    config = function()
+      local custom_golang = require("lint").linters.golangcilint
+      custom_golang.append_fname = false
+      custom_golang.args = {
+          "run",
+          "--out-format",
+          "json",
+          "--build-tags",
+          "exclude_graphdriver_btrfs,btrfs_noversion,exclude_graphdriver_devicemapper,containers_image_openpgp,remote",
+      }
+
+      require("lint").linters_by_ft = {
+        go = { "golangcilint" },
+      }
+
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
+}
