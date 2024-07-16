8b0c319f6c8505453c60c8f4a03bf7b2736c5c93
Chris Kyrouac <ckyrouac@redhat.com>
2024-07-16 13:39:03 -0400

    Add copilot chat plugin to nvim

---
+++ nvim/lua/ckyrouac/plugins/copilot-chat.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    cond = true,
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = false, -- Enable debugging
+      context = "buffers",
+      -- See Configuration section for rest
+    },
+    config = function()
+      require("CopilotChat").setup {
+        debug = false,
+      }
+
+      ---@diagnostic disable-next-line: lowercase-global
+      function _openChatWindow()
+        local chat = require("CopilotChat")
+        chat.toggle({
+          window = {
+            layout = 'float',
+            title = 'Copilot Chat',
+            width = 0.8,
+            height = 0.8,
+          },
+        })
+      end
+
+      vim.api.nvim_set_keymap('n', '<leader>hc', "<cmd>lua _openChatWindow()<CR>", {noremap = true, silent = true})
+    end,
+  },
+}
