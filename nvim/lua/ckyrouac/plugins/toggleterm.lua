906edd56efe083bd2e28d7c94c8bf468a0af13be
Chris Kyrouac <ckyrouac@redhat.com>
2024-07-16 13:40:11 -0400

    Remove dropdown toggle term

--- nvim/lua/ckyrouac/plugins/toggleterm.lua
+++ nvim/lua/ckyrouac/plugins/toggleterm.lua
@@ -42,56 +42,6 @@ return {
         "<cmd>lua _lazygit_toggle()<CR>",
         { noremap = true, silent = true, desc = "Lazy Git Terminal" }
       )
-
-      -- dropdown
-      local dropdown = Terminal:new({
-        hidden = true,
-        start_in_insert = true,
-        insert_mappings = true,
-        terminal_mappings = true,
-        direction = "float",
-        shell = vim.o.shell,
-        name = "dropdown",
-        on_open = function()
-          vim.cmd("startinsert!")
-          vim.api.nvim_buf_set_keymap(
-            0,
-            "t",
-            "<A-w>",
-            [[<C-\><C-o>:lua _dropdown_toggle()<cr>]],
-            { noremap = true, silent = true }
-          )
-          vim.api.nvim_buf_set_keymap(
-            0,
-            "t",
-            "<A-4>",
-            [[<C-\><C-o>:lua _dropdown_toggle()<cr>]],
-            { noremap = true, silent = true }
-          )
-          vim.api.nvim_buf_set_keymap(
-            0,
-            "t",
-            "<esc><esc>",
-            [[<C-\><C-o>:lua _dropdown_toggle()<cr>]],
-            { noremap = true, silent = true }
-          )
-          vim.api.nvim_buf_set_keymap(
-            0,
-            "t",
-            "<leader>t",
-            [[<C-\><C-o>:lua _dropdown_toggle()<cr>]],
-            { noremap = true, silent = true }
-          )
-        end,
-      })
-
-      ---@diagnostic disable-next-line: lowercase-global
-      function _dropdown_toggle()
-        dropdown:toggle()
-      end
-
-      vim.api.nvim_set_keymap("n", "<A-4>", "<cmd>lua _dropdown_toggle()<CR>", { silent = true })
-      vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>lua _dropdown_toggle()<CR>", { silent = true })
     end,
   },
 }
