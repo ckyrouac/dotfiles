1991a094728803d1f4d21629a3f0af5d2308a9b9
Chris Kyrouac <ckyrouac@redhat.com>
2024-10-09 10:37:33 -0400

    Update gitsigns glyphs

--- nvim/lua/ckyrouac/plugins/gitsigns.lua
+++ nvim/lua/ckyrouac/plugins/gitsigns.lua
@@ -8,14 +8,13 @@ return {
       "VeryLazy",
     },
     opts = {
-      -- See `:help gitsigns.txt`
       signs = {
-        add = { text = "⎮" },
-        change = { text = "⎮" },
-        delete = { text = "⎮" },
-        topdelete = { text = "⎮" },
-        changedelete = { text = "⎮" },
-        untracked = { text = "⎮" },
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "┃" },
+        topdelete = { text = "┃" },
+        changedelete = { text = "┃" },
+        untracked = { text = "┃" },
       },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
