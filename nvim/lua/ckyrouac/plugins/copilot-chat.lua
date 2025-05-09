59e1774cf14c36716c729ea5e55a53c58dae9345
ckyrouac <ckyrouac@redhat.com>
2025-05-09 15:48:01 -0400

    Initial wca provider test

--- nvim/lua/ckyrouac/plugins/copilot-chat.lua
+++ nvim/lua/ckyrouac/plugins/copilot-chat.lua
@@ -15,6 +15,68 @@ return {
     config = function()
       require("CopilotChat").setup {
         debug = false,
+        -- model = 'ibm/granite-8b-code-instruct',
+        providers = {
+          watson = {
+              prepare_input = function(inputs, opts)
+                local body = {
+                  message = inputs,
+                }
+
+                return body
+              end,
+
+              prepare_output = function(output)
+                local content = output.response.messages.content
+
+                return {
+                  content = content,
+                  finish_reason = "",
+                  total_tokens = "",
+                  references = "",
+                }
+              end,
+
+              get_headers = function()
+                  -- TODO: cache the key until it expires
+                  local api_key = assert(os.getenv('WCA_API_KEY'), 'WCA_API_KEY env not set')
+
+                  local response, err = require('CopilotChat.utils').curl_post('https://iam.cloud.ibm.com/identity/token', {
+                      headers = {
+                        ['Content-Type'] = 'application/x-www-form-urlencoded',
+                      },
+                      body = 'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=' .. api_key,
+                      json_response = true,
+                  })
+
+                  if err then
+                      error(err)
+                  end
+
+                  print("token: " .. response.body.access_token)
+
+                  return {
+                      Authorization = 'Bearer ' .. response.body.access_token,
+                      ['Content-Type'] = 'multipart/form-data',
+                  }
+              end,
+
+              get_models = function()
+                  return {
+                    {
+                      id = "ibm/granite-8b-code-instruct",
+                      name = "ibm/granite-8b-code-instruct",
+                    }
+                  }
+              end,
+
+              embed = require('CopilotChat.config.providers').copilot.embed,
+
+              get_url = function()
+                  return 'https://api.dataplatform.cloud.ibm.com/v2/wca/core/chat/text/generation'
+              end,
+          },
+        },
       }
 
       ---@diagnostic disable-next-line: lowercase-global
