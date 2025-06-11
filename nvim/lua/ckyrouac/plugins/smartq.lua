dc43d60121755625a07ab24ed8e6db31aab1043f
ckyrouac <ckyrouac@redhat.com>
2025-06-11 11:15:26 -0400

    Smartq tweaks

--- nvim/lua/ckyrouac/plugins/smartq.lua
+++ nvim/lua/ckyrouac/plugins/smartq.lua
@@ -4,6 +4,9 @@ return {
     cond = true,
     init = function()
       vim.g.smartq_default_mappings = 0
+      vim.g.smartq_exclude_filetypes = {'fugitive'}
+      vim.g.smartq_no_exit = 1
+      vim.g.smartq_auto_close_splits = 1
     end,
     config = function() end,
   },
