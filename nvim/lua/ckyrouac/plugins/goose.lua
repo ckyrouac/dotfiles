9a6ff5c22e6e62be22c046f9f4edeaf80ca9581d
ckyrouac <ckyrouac@redhat.com>
2025-06-18 12:08:45 -0400

    Fix goose <leader>ac bind

--- nvim/lua/ckyrouac/plugins/goose.lua
+++ nvim/lua/ckyrouac/plugins/goose.lua
@@ -21,7 +21,6 @@ return {
               diff_open = '<leader>ad',              -- Opens a diff tab of a modified file since the last goose prompt
               diff_next = '<leader>a]',              -- Navigate to next file diff
               diff_prev = '<leader>a[',              -- Navigate to previous file diff
-              diff_close = '<leader>ac',             -- Close diff view tab and return to normal editing
               diff_revert_all = '<leader>ara',       -- Revert all file changes since the last goose prompt
               diff_revert_this = '<leader>art',      -- Revert current file changes since the last goose prompt
           },
