d99bbe7a9e45df1a21bc7b3246817dbbcd727af3
ckyrouac <ckyrouac@redhat.com>
2025-07-11 10:40:40 -0400

    Try out minuet for inline completions

--- nvim/lua/ckyrouac/plugins/minuet-ai.lua
+++ nvim/lua/ckyrouac/plugins/minuet-ai.lua
@@ -1,60 +1,65 @@
 return {
   {
-    'milanglacier/minuet-ai.nvim',
-    cond = false,
+    "milanglacier/minuet-ai.nvim",
+    cond = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    config = function ()
-      require('minuet').setup {
-        provider = 'openai_fim_compatible',
+    config = function()
+      require("minuet").setup({
+        cmp = {
+          enable_auto_complete = false,
+        },
+        blink = {
+          enable_auto_complete = false,
+        },
+        provider = "gemini",
+        context_window = 20000,
+        request_timeout = 4,
+        throttle = 3000,
+        debounce = 1000,
+        n_completions = 1,
         provider_options = {
-          openai_fim_compatible = {
-            model = 'qwen2.5-coder:7b',
-            end_point = 'http://localhost:11434/v1/completions',
-            api_key = function() return 'sk-xxxx' end,
-            stream = false,
-            name = 'Mistral',
+          gemini = {
+            api_key = "GEMINI_API_KEY",
+            model = "gemini-2.5-flash",
+            optional = {
+              generationConfig = {
+                maxOutputTokens = 256,
+                -- When using `gemini-2.5-flash`, it is recommended to entirely
+                -- disable thinking for faster completion retrieval.
+                thinkingConfig = {
+                  thinkingBudget = 0,
+                },
+              },
+              safetySettings = {
+                {
+                  category = "HARM_CATEGORY_DANGEROUS_CONTENT",
+                  threshold = "BLOCK_ONLY_HIGH",
+                },
+              },
+            },
           },
         },
-        request_timeout = 10,
-        context_window = 768,
         virtualtext = {
-          auto_trigger_ft = {'rust', 'lua', 'bash'},
+          auto_trigger_ft = { "*" },
+          auto_trigger_ignore_ft = {},
           keymap = {
             -- accept whole completion
-            accept = '<A-a>',
+            accept = "<C-y>",
             -- accept one line
-            accept_line = '<A-y>',
+            accept_line = "<A-a>",
             -- accept n lines (prompts for number)
             -- e.g. "A-z 2 CR" will accept 2 lines
-            accept_n_lines = '<A-z>',
+            accept_n_lines = "<A-z>",
             -- Cycle to prev completion item, or manually invoke completion
-            prev = '<A-[>',
+            prev = "<A-[>",
             -- Cycle to next completion item, or manually invoke completion
-            next = '<A-]>',
-            dismiss = '<A-e>',
+            next = "<A-]>",
+            dismiss = "<A-e>",
           },
         },
-      }
-      --
-      -- llm.setup({
-      --   api_token = nil, -- cf Install paragraph
-      --   model = "granite-code:8b", -- the model ID, behavior depends on backend
-      --   backend = "ollama", -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
-      --   url = "http://localhost:11434", -- the http url of the backend
-      --   request_body = {
-      --     options = {
-      --       temperature = 0.2,
-      --       top_p = 0.95,
-      --     }
-      --   },
-      --   accept_keymap = "<C-y>",
-      --   tls_skip_verify_insecure = true,
-      --   context_window = 5000, -- max number of tokens for the context window
-      --   enable_suggestions_on_startup = true,
-      --   enable_suggestions_on_files = "*.lua", -- pattern matching syntax to enable suggestions on specific files, either a string or a list of strings
-      -- })
+      })
     end,
   },
 }
